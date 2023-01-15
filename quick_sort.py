1b2cb3f81590f8fad4789a2890250a37437952a0
Fardin <60337534+FardinHash@users.noreply.github.com>
2023-01-15 19:42:15 +0600

    quick

--- quick_sort.py
+++ quick_sort.py
@@ -1,24 +1,24 @@
 #Quick sorting
 
-def partition(arr,low,high):
-    i = ( low-1 )         
+def partition(arr, low, high):
+    i = ( low - 1 )         
     pivot = arr[high]
  
-    for j in range(low , high):
+    for j in range(low, high):
         if   arr[j] <= pivot:
-            i = i+1
-            arr[i],arr[j] = arr[j],arr[i]
+            i = i + 1
+            arr[i], arr[j] = arr[j], arr[i]
  
-    arr[i+1],arr[high] = arr[high],arr[i+1]
+    arr[i+1], arr[high] = arr[high], arr[i + 1]
 
-    return ( i+1 )
+    return(i + 1)
 
-def quick_sort(arr,low,high):
+def quick_sort(arr, low, high):
     if low < high:
-        pi = partition(arr,low,high)
+        pi = partition(arr, low, high)
  
         quick_sort(arr, low, pi-1)
-        quick_sort(arr, pi+1, high)
+        quick_sort(arr, pi + 1, high)
 
     return arr
 
